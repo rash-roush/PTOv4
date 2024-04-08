f4412585b0dec1c21e577ffc72bbd1dbef9b6504
rasha ali <55467025+rash-roush@users.noreply.github.com>
2024-04-07 22:46:45 -0400

    replace all w new changes test

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -35,55 +35,63 @@
             pictureBox1 = new PictureBox();
             button_login = new Button();
             button_clear = new Button();
+            checkedListBox1 = new CheckedListBox();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // txt_username
             // 
-            txt_username.Location = new Point(480, 72);
+            txt_username.Location = new Point(384, 58);
+            txt_username.Margin = new Padding(2, 2, 2, 2);
             txt_username.Name = "txt_username";
-            txt_username.Size = new Size(208, 31);
+            txt_username.Size = new Size(167, 27);
             txt_username.TabIndex = 0;
             // 
             // txt_password
             // 
-            txt_password.Location = new Point(480, 141);
+            txt_password.Location = new Point(384, 113);
+            txt_password.Margin = new Padding(2, 2, 2, 2);
             txt_password.Name = "txt_password";
-            txt_password.Size = new Size(208, 31);
+            txt_password.Size = new Size(167, 27);
             txt_password.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(316, 68);
+            label1.Location = new Point(253, 54);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(122, 25);
+            label1.Size = new Size(101, 20);
             label1.TabIndex = 2;
             label1.Text = "Employee ID :";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(316, 144);
+            label2.Location = new Point(253, 115);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(101, 25);
+            label2.Size = new Size(81, 20);
             label2.TabIndex = 3;
             label2.Text = "Password : ";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.user_200;
-            pictureBox1.Location = new Point(43, 41);
+            pictureBox1.Location = new Point(34, 33);
+            pictureBox1.Margin = new Padding(2, 2, 2, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(205, 213);
+            pictureBox1.Size = new Size(164, 170);
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
             // button_login
             // 
-            button_login.Location = new Point(553, 220);
+            button_login.Location = new Point(443, 350);
+            button_login.Margin = new Padding(2, 2, 2, 2);
             button_login.Name = "button_login";
-            button_login.Size = new Size(112, 34);
+            button_login.Size = new Size(90, 27);
             button_login.TabIndex = 5;
             button_login.Text = "Login";
             button_login.UseVisualStyleBackColor = true;
@@ -91,19 +99,39 @@
             // 
             // button_clear
             // 
-            button_clear.Location = new Point(370, 220);
+            button_clear.Location = new Point(297, 350);
+            button_clear.Margin = new Padding(2, 2, 2, 2);
             button_clear.Name = "button_clear";
-            button_clear.Size = new Size(112, 34);
+            button_clear.Size = new Size(90, 27);
             button_clear.TabIndex = 6;
             button_clear.Text = "Clear";
             button_clear.UseVisualStyleBackColor = true;
             button_clear.Click += button_clear_Click;
             // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(375, 189);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(176, 92);
+            checkedListBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(253, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 8;
+            label3.Text = "label3";
+            // 
             // DBHandler
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(727, 282);
+            ClientSize = new Size(582, 412);
+            Controls.Add(label3);
+            Controls.Add(checkedListBox1);
             Controls.Add(button_clear);
             Controls.Add(button_login);
             Controls.Add(pictureBox1);
@@ -111,6 +139,7 @@
             Controls.Add(label1);
             Controls.Add(txt_password);
             Controls.Add(txt_username);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "DBHandler";
             Text = "DB Handler";
             Load += DBHandler_Load;
@@ -128,5 +157,7 @@
         private PictureBox pictureBox1;
         private Button button_login;
         private Button button_clear;
+        private CheckedListBox checkedListBox1;
+        private Label label3;
     }
 }
