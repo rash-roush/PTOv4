09ed4021e2f2c510d5f98227a87b89135142ae3a
rasha ali <55467025+rash-roush@users.noreply.github.com>
2024-04-12 10:04:21 -0400

    Delete dbo.LOGIN.sql

--- dbo.LOGIN.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[LOGIN] (
-    [EmpID]         INT        NOT NULL,
-    [EmpDepartment] NCHAR (10) NULL,
-    [EmpSalary]     FLOAT (53) NULL,
-    [EmpPos]        NCHAR (10) NULL,
-    [Password] NCHAR(10) NULL, 
-    PRIMARY KEY CLUSTERED ([EmpID] ASC)
-);
-
